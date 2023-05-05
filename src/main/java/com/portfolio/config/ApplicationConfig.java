925d9819f718fddb7c55acc06409ac16cf5339e0
Rocio Vargas <vargas.rocio@hotmail.com.ar>
2023-05-04 22:51:26 -0300

    update configuration

--- src/main/java/com/portfolio/config/ApplicationConfig.java
+++ src/main/java/com/portfolio/config/ApplicationConfig.java
@@ -4,6 +4,7 @@ import com.portfolio.repository.AccountRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -15,6 +16,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableJpaRepositories(basePackages = "com.portfolio.repository")
 public class ApplicationConfig {
 
     private final AccountRepository accountRepository;
